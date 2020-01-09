0b656d98a40ba8ca14f5c6a34d712118aba01bec
Skelmis <Skelmis.craft@gmail.com>
2020-01-10 00:01:33 +1300

    .

---
+++ cogs/spam_checks.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+import cogs._json
+import cogs.util_functions
+
+class SpamChecks(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def checkmessage(self, ctx, *, args):
+        await UserMessageMeetsThreshold(self.bot, ctx.guild, ctx.author, args)
+
+async def UserMessageMeetsThreshold(bot, guild, user, message):
+    data = cogs._json.read_json('config')
+    if not str(guild.id) in data['configs']:
+        guildSetting = data['configs']['default']['userMessagesMinThresholdForSpam']
+        await cogs.util_functions.SetupGuildDefaultConfig(guild)
+    else:
+        guildSetting = data['configs'][str(guild.id)]['userMessagesMinThresholdForSpam']
+    messageContent = message
+    if len(messageContent) > guildSetting:
+        #do stuff
+        pass
+
+
+def setup(bot):
+    bot.add_cog(SpamChecks(bot))
